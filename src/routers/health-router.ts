57e45979bbb0da0526cd126095d714d335ed0840
Daniel <daanilo2497@gmail.com>
2023-05-16 23:27:40 -0300

    delete health router and controller

--- src/routers/health-router.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { healthController } from '@src/controllers';
-
-const healthRouter = express.Router();
-healthRouter.get('/health', healthController.health);
-
-export { healthRouter };
