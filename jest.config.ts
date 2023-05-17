4abdd46e05f39466d7a418b0be1c72affc449e7a
Daniel <daanilo2497@gmail.com>
2023-05-16 23:25:25 -0300

    Config path mappings

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@routers/(.*)$': '<rootDir>/src/routers/$1',
+    '^@schemas/(.*)$': '<rootDir>/src/schemas/$1',
+  },
 };
 
 export default config;
