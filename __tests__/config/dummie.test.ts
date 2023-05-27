00b6a64d9b45844f72dc5b3ff0deb06d80587c16
Daniel <daanilo2497@gmail.com>
2023-05-27 16:05:16 -0300

    fix dummy test

--- __tests__/config/dummie.test.ts
+++ __tests__/config/dummie.test.ts
@@ -1,10 +1,10 @@
 import { CreateSubscriberSchema } from "@src/controllers/schemas";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 const obj = {
   body: {
     user_id: "               234  ",
-    device_id: "899"
+    device_token: "899"
   },
   params: {
     pepe: 'da'
@@ -14,14 +14,13 @@ const obj = {
 const a = z.string().trim();
 const b = "  pepe  ";
 const c = a.parse("              pepe  ");
-const result2 = CreateSubscriberSchema.parse(obj);
 
 test('dummie test', () => {
   const result = 0;
-  console.log(result2);
+  
   console.log(obj);
   console.log(c);
   console.log(b)
   expect(result).toBe(0);
-
+  expect(() => CreateSubscriberSchema.parse(obj)).toThrow(ZodError);
 });
