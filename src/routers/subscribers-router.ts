5417ed1730bafb27dafb50c164a9c2d249ef8ab4
Daniel <daanilo2497@gmail.com>
2023-06-05 23:19:22 -0300

    Add doc to patch subscribers endpoint

--- src/routers/subscribers-router.ts
+++ src/routers/subscribers-router.ts
@@ -28,6 +28,28 @@ const subscribersRouter = express.Router();
 
 /** 
  * @swagger
+ * components:
+ *   schemas:
+ *     UpdateSubscriber:
+ *       type: object
+ *       properties:
+ *         user_id: 
+ *           type: string
+ *           description: User ID of the new subscriber
+ *         device_token:
+ *           type: string
+ *           description: Device Expo Token that identifies the user device
+ *         subscribed:
+ *           type: bool
+ *           description: Set whether the notification is send to the user app or not
+ *       example:
+ *         user_id: AAFF789K
+ *         device_token: ExponentPushToken[m5qIwWEA9E-LtduZOV4U9h]
+ *         subscribed: false
+ */
+
+/** 
+ * @swagger
  * /api/v1/subscribers:
  *   post:
  *     summary: Create a new subscriber
@@ -59,6 +81,36 @@ const subscribersRouter = express.Router();
  */
 subscribersRouter.post('/subscribers', validateRequest(CreateSubscriberSchema), subscriberController.createSubscriber);
 
+/** 
+ * @swagger
+ * /api/v1/subscribers/{id}:
+ *   patch:
+ *     summary: Update a subscriber
+ *     tags:
+ *       - UpdateSubscriber
+ *     requestBody:
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#components/schemas/UpdateSubscriber'
+ *     responses:
+ *       200:
+ *         description: Subscriber updated
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#components/schemas/SuccessResponse'
+ *       500:
+ *         description: Internal Server Error - An error occurred while processing the request.
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#components/schemas/ErrorResponse'
+ * 
+ */
 subscribersRouter.patch('/subscribers/:id', validateRequest(PatchSubscriberSchema), subscriberController.patchSubscriber);
 
 export { subscribersRouter };
